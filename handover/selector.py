56f5c8768038438c771ab22cb65f510fed25035d
Parth Patel <parthpatel2019@gmail.com>
2022-10-24 16:48:40 -0700

    Re-worked Selection Bug
    Used the difference in timestamps to help with the gradient calculations

--- handover/selector.py
+++ handover/selector.py
@@ -7,9 +7,8 @@ class AccessPointSelector:
     Holds logic for choosing between different AccessPoints
     """
 
-    def predict_future_quality(self, access_point: AccessPoint, name="rsrq"):
+    def predict_future_quality(self, access_point: AccessPoint, current_timestamp: int, name="rsrq"):
         # predicts the quality of the access point in the next time step
-        
         # Select which column we are using for prediction
         if name == "rsrp":
             extractor = lambda x: x.rsrp
@@ -31,19 +30,25 @@ class AccessPointSelector:
             return values[0]
 
         # Compute average slope
-        gradients = np.gradient(np.array(values), np.array(timestamps))
+        # If len(values) != len(timestamps), chance to get divide by 0 error
+        # We avoid that situation here
+        if(len(np.unique(np.array(timestamps))) != len(np.array(values))):
+            gradients = np.gradient(np.array(values))
+        else:
+            gradients = np.gradient(np.array(values), np.array(timestamps))
         avg_gradient = np.mean(gradients)
-        predicted_future_quality = values[-1] + avg_gradient
+        predicted_future_quality = values[-1] + (timestamps[-1] - current_timestamp) * avg_gradient
 
         return predicted_future_quality
 
-    def choose(self, access_points):
+    def choose(self, access_points, current_timestamp):
         best_access_point = None
         best_value = -100000
         scores = []
 
         for access_point in access_points: 
-            quality = self.predict_future_quality(access_point)
+            quality = self.predict_future_quality(access_point, current_timestamp)
+            print("Pci: ", access_point.pci, " - Predicted RSRQ: ", quality)
             if quality > best_value: 
                 best_value = quality
                 best_access_point = access_point
