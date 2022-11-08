c92a0f591cc079302ac3fb285d81159fde20f381
Albert Stanley <albertjstanley@gmail.com>
2022-11-07 17:54:37 -0800

    fix accuracy prediction bug

--- handover/simulator.py
+++ handover/simulator.py
@@ -13,6 +13,7 @@ class Simulator:
         self.ap_manager = AccessPointManager()
         self.ap_selector = AccessPointSelector()
         self.timetamp_to_registered_pci = {}
+        self.prev_chosen_ap = None # AP previously selected
         self.correct_predictions = 0.0
         self.total_predictions = 0.0
 
@@ -21,8 +22,16 @@ class Simulator:
         print(f"Correctly Predicted: {self.correct_predictions} out of total: {self.total_predictions}")
         print(f"Accuracy: {(self.correct_predictions/self.total_predictions):2.2%}")
 
-    def _evaluate_last_timestep(self, access_points):
+    def _evaluate_timestep(self, access_points):
         registered_pci = self.timetamp_to_registered_pci[self.cur_timestamp]
+
+        # Note accuracy if we have prediction from the past step
+        if (self.prev_chosen_ap):
+            if(registered_pci == self.prev_chosen_ap.pci):
+                self.correct_predictions = self.correct_predictions + 1
+            self.total_predictions = self.total_predictions + 1
+
+        # Make predictions for the next timestep
         chosen_access_point, time_until_threshold = self.ap_selector.choose(access_points, self.cur_timestamp)
         
         print(f"Currently Registered: {registered_pci}")
@@ -30,9 +39,9 @@ class Simulator:
         print(f"Chosen: {chosen_access_point.pci}")
         print(f"Time until Threshold Reached: {time_until_threshold}")
         print("______________________")
-        if(registered_pci == chosen_access_point.pci):
-            self.correct_predictions = self.correct_predictions + 1
-        self.total_predictions = self.total_predictions + 1
+
+        # Setup prev chosen ap for next round
+        self.prev_chosen_ap = chosen_access_point
 
     def run(self):
         """
@@ -42,10 +51,10 @@ class Simulator:
 
         access_points = set()
 
-        for i, row in self.df.iterrows():
+        for _, row in self.df.iterrows():
             # Keep different timestamp sections in chunks
             if self.cur_timestamp != None and self.cur_timestamp != row.mTimeStamp: 
-                self._evaluate_last_timestep(access_points)
+                self._evaluate_timestep(access_points)
                 access_points = set()
             self.cur_timestamp = row.mTimeStamp
 
@@ -57,7 +66,7 @@ class Simulator:
             ap.add_data(row.mTimeStamp, AccessPointData(row.ss, row.rsrp, row.rsrq))
             access_points.add(ap)
 
-        self._evaluate_last_timestep(access_points)
+        self._evaluate_timestep(access_points)
 
         self.print_metrics()
 
